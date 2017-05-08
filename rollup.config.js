dfeb7001cdbfd43b84e6d72d56accfadee7f842e
Louis Chan <louischan@oursky.com>
2017-05-08 13:57:44 +0800

    Mark all dependencies as external when building cjs bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
@@ -5,17 +6,20 @@ import uglify from 'rollup-plugin-uglify';
 
 const format = process.env.format;
 const minify = format === 'umd';
+const packageJson = JSON.parse(fs.readFileSync('./package.json'));
 
+let external;
 let middleName;
 if (format === 'cjs') {
   middleName = 'cjs';
+  external = Object.keys(packageJson.dependencies || {});
 } else if (format === 'umd') {
   middleName = 'min';
+  external = undefined;
 }
 
 const plugins = [
   resolve({
-    preferBuiltins: false, // we use querystring and url from npm
   }),
   commonjs({
     include: 'node_modules/**',
@@ -32,6 +36,7 @@ export default {
   entry: 'src/index.js',
   dest: `dist/bundle.${middleName}.js`,
   format,
+  external,
   moduleName: 'SkygearEventTracking',
   moduleContext: {
     'node_modules/whatwg-fetch/fetch.js': 'window',
